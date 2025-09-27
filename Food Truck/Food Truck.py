47f79fb5a4bba12945988602f221615b6bb840c4
HannahAKN <nguessanhannah@gmail.com>
2025-09-27 16:57:57 +0200

    Made a file to practice If/elif/else statement's

---
+++ Food Truck/Food Truck.py
@@ -0,0 +1,5 @@
+#This program is to practice if/elif/else statements. This program will give you the menu of an food truck. It will let you choose Items and then give you the total for your order.
+
+print("Welcome to the Lusty Loot Food Truck!")
+print("What can we get for you today?")
+

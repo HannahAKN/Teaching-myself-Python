25d9d4fcd9d66286fb6d21e38491c6419801fa15
HannahAKN <nguessanhannah@gmail.com>
2025-10-02 21:37:40 +0200

    Wrote introduction to the game

--- Choose your own adventure game/Choose your own adventure.py
+++ Choose your own adventure game/Choose your own adventure.py
@@ -1,1 +1,6 @@
 print("Welcome Too The Adventure!")
+
+print("""This is an choose you own adventure game. 
+That means that this is a 100% text based game, where you choose your paths you wish to take.
+That can be anything from walking left or right, to choosing to kill a beast or not. 
+This game have multiple endings, and it's up too you to discover them all!""")

10fcf9af9dc410a24d6a75e2f02e2c0850c9b0c0
HannahAKN <nguessanhannah@gmail.com>
2025-10-07 21:55:37 +0200

    Learning how to import things

---
+++ Learning how to import things/Document_to_import.py
@@ -0,0 +1,3 @@
+#This document is simply to learn how to import from things
+
+favourite_number = 202020202020
---
+++ Learning how to import things/How_to_import.py
@@ -0,0 +1,4 @@
+import Document_to_import
+
+#You have to type the name of the document you want to import from, and then the name of the variable
+print(Document_to_import.favourite_number)
